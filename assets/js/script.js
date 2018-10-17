4b6b62d8f0777e878471ba481ac1c57cb58ac1df
George Bernardsha <you@example.com>
2018-10-17 21:54:57 +0530

    Add function to find elelment is in viewport or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,23 @@ $(document).ready(function () {
       $('#progressbar'+i).append(progressbars);
     }
 
+    function isElementInViewport (el) {
+
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+  }
+
   function animeBar() {
     for (var i = 0; i < progressBarData.length; i++) {
       $('#progressbar'+i).remove();
@@ -190,7 +207,7 @@ $(document).ready(function () {
 
     // consumer desire animation
     var consumer_top = $('.desire-stats').offset().top;
-    if (consumer_top < (topOfWindow + 500)) {
+    if (isElementInViewport($('.consumer-desire h3'))) {
       $('.desire-stats').addClass("anime-consumer-desire");
       if (!counterAnimateFlag) {
         counterAnimateFlag = true;
