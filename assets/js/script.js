3ce4339417eb6c509459bd62ca842888e924d58f
Sudharsan <sudharsant@prdxn.com>
2018-10-18 15:56:58 +0530

    isElementInViewport function used

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
       subtracted_val = government_ul_top.top - government_top.top
       $('.hand-money-bank-image').css('top', subtracted_val + 34);
     } else {
-      $('.hand-money-bank-image').css('cssText', 'top', 'unset')
+      $('.hand-money-bank-image').css('cssText', 'top', 'unset');
     }
 
     // Align offering section triangle
@@ -162,11 +162,14 @@ $(document).ready(function () {
     var lightning_banner = $('.banner').offset().top;
     var topOfWindow = $(window).scrollTop();
 
-    $('.lightning').addClass("anim-lightning");
+    if (isElementInViewport($('.lightning-middle'))) {
+      $('.lightning').addClass("anim-lightning");
+    }
+
 
     // Car animation
     var tempo_right = $('.tempo-to-right').offset().top;
-    if (tempo_right < (topOfWindow + 700)) {
+    if (isElementInViewport($('.government-description'))) {
       $('.car-to-left').addClass("car-left");
       $('.car-to-right').addClass("car-right");
       $('.tempo-to-right').addClass("tempo-right");
@@ -197,7 +200,7 @@ $(document).ready(function () {
 
     // Death animation
     var sky_top = $('.sky').offset().top;
-    if (sky_top < (topOfWindow + 700)) {
+    if (isElementInViewport($('.ill-health .common-text-style'))) {
       $('.half-round').addClass("anime-half-round");
       $('.round').addClass("anime-roud");
       $('.sky').addClass("sky-anime");
@@ -206,19 +209,19 @@ $(document).ready(function () {
 
     // Graph animation
     var bar_top = $('.bar').offset().top;
-    if (bar_top < (topOfWindow + 780)) {
+    if (isElementInViewport($('.polution-stats .common-text-style'))) {
       $('.bar').addClass("anime-bar");
     }
 
     // on the road graph animation
     var on_road_top = $('.on-the-road-graph').offset().top;
-    if (on_road_top < (topOfWindow+700)) {
+    if (isElementInViewport($('.charging-point-info'))) {
       $('.on-the-road-graph').addClass("anime-on-the-road-graph");
     }
 
     // consumer desire animation
     var consumer_top = $('.desire-stats').offset().top;
-    if (isElementInViewport($('.consumer-desire h3'))) {
+    if (isElementInViewport($('.consumer-info'))) {
       $('.desire-stats').addClass("anime-consumer-desire");
       if (!counterAnimateFlag) {
         counterAnimateFlag = true;
