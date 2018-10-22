adfef6ce03ae799ee0975bcdc5eddce667bd5d8e
George Bernardsha <you@example.com>
2018-10-22 12:00:18 +0530

    Add style to on the road graph on responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,7 +83,7 @@ $(document).ready(function () {
         fetch_url_val:fetch_url_val,
         percentage: progressBarData[i].value,
         fillBackgroundColor: progressColors[i],
-        height: $(window).width() >= 768 ? 55 : 65
+        height: $(window).width() >= 768 ? 55 : 58
       });
       $('#progressbar' + i + ' .progressName').text(progressBarData[i].name);
     }
